68a54f052b9898418980fb3e3702960d7bcaf910
Raul Gonzalez <gabriel71871@gmail.com>
2023-05-01 02:01:04 -0600

    Actualizacion

--- Practica 2.py
+++ Practica 2.py
@@ -5,24 +5,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('C:/Users/raulh/OneDrive/Documentos/Tareas/Mineria de datos/lol_dataframe2.csv')
-# Visualizamos nuestro dataframe por sus categorias
+# Visualizamos nuestro dataframe por sus categorias\
 data.info()
+print('-------------------------------------------------------------------------------\n')
 
-# Calculamos la suma individual de las columnas gameDuration, queueId y seasonId
-print ('La suma individual de las columnas gameDuration, queueId y seasonId es: \n', data[['gameDuration', 'queueId', 'seasonId']].sum()) 
+# Calculamos la suma individual de las columnas blueWins y redWins
+print ('La suma de las Victorias del Equipo Azul es:', data['blueWins'].sum(), '\nLa suma de las Victorias del Equipo Red es:', data['redWins'].sum())
+print('-------------------------------------------------------------------------------\n')
 
-# Creamos una funcion donde sacamos la media de las columnas: gameDuration, queueId y seasonId
+# Buscamos la moda de las columnas: blueWardsPlaced, redWardsPlaced, blueTotalGold, redTotalGold
+print('El valor de la moda de Wards del equipo azul colocados es:', data['blueWardsPlaced'].mode(),'\nEl valor de la moda de Wards del Equipo Rojo colocados es:', 
+      data['redWardsPlaced'].mode(), '\nEl valor de la moda del Total de Oro ganado del Equipo Azul es:', data['blueTotalGold'].mode(), 
+      '\nEl valor de la moda del Total de Oro ganado del Equipo Red es:', data['redTotalGold'].mode())
+print('-------------------------------------------------------------------------------\n')
+
+# Buscamos el maximo valor de las columnas: blueWardsPlaced, redWardsPlaced, blueTotalGold, redTotalGold
+print('El maximo valor de Wards del equipo azul colocados es:', data['blueWardsPlaced'].max(),'\nEl maximo valor de Wards del Equipo Rojo colocados es:', 
+      data['redWardsPlaced'].max(), '\nEl maximo valor del Total de Oro ganado del Equipo Azul es:', data['blueTotalGold'].max(), 
+      '\nEl maximo valor del Total de Oro ganado del Equipo Red es:', data['redTotalGold'].max())
+print('-------------------------------------------------------------------------------\n')
+
+# Buscamos el minimo valor de las columnas: blueWardsPlaced, redWardsPlaced, blueTotalGold, redTotalGold
+print('El minimo valor de Wards del equipo azul colocados es:', data['blueWardsPlaced'].min(),'\nEl minimo valor de Wards del Equipo Rojo colocados es:', 
+      data['redWardsPlaced'].min(), '\nEl minimo valor del Total de Oro ganado del Equipo Azul es:', data['blueTotalGold'].min(), 
+      '\nEl minimo valor del Total de Oro ganado del Equipo Red es:', data['redTotalGold'].min())
+print('-------------------------------------------------------------------------------\n')
+
+# Creamos una funcion donde sacamos la media de las columnas: blueWardsPlaced, redWardsPlaced, blueTotalGold, redTotalGold
 def media(data):
     mean = data.mean()
     return mean
 
-sel_cols = ['gameDuration', 'queueId', 'seasonId']
+sel_cols = ['blueWardsPlaced', 'redWardsPlaced', 'blueTotalGold', 'redTotalGold']
 
 b = data[sel_cols]
 
-gameDuration, queueId, seasonId = b.apply(media)
+blueWardsPlaced, redWardsPlaced, blueTotalGold, redTotalGold = b.apply(media)
 
-print('La media de gameDuration, queueId, seasonId es:', gameDuration, queueId, seasonId)
+print('La media de Wards del equipo azul colocados es:', blueWardsPlaced,'\nLa media de Wards del Equipo Rojo colocados es:', redWardsPlaced,
+      '\nLa media del Total de Oro ganado del Equipo Azul es:', blueTotalGold, '\nLa media del Total de Oro ganado del Equipo Red es:', redTotalGold )
+print('-------------------------------------------------------------------------------\n')
 
 # Creamos dos funciones, una para sacar la Varianza y otra para la Desviacion estandar de las columnas: gameDuration, queueId y seasonId
 def calc_mean(series):
