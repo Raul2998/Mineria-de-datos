090fb9b752333d033df58856f7a53625c2b9e601
Raul Gonzalez <gabriel71871@gmail.com>
2023-05-01 14:30:00 -0600

    AC

--- Practica3/Practica3.py
+++ Practica3/Practica3.py
@@ -9,8 +9,13 @@ from Practica2.Practica2 import *
 import Practica2.Practica2 as P2
 
 P2.data
-P2.sel_cols
+P2.gold
+P2.wards
 
-for i in range(0,3):
-    plt.scatter(x = P2.data['gameId'], y = P2.data[P2.sel_cols[i]])
+for i in range(0,2):
+    plt.scatter(x = P2.data['gameId'], y = P2.data[P2.gold[i]])
+plt.show()
+
+for i in range(0,2):
+    plt.scatter(x = P2.data['gameId'], y = P2.data[P2.wards[i]])
 plt.show()
