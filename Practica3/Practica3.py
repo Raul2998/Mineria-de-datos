9ad57514021c7713892dac521fbf89ae5d7c5fff
Raul Gonzalez <gabriel71871@gmail.com>
2023-05-04 14:47:51 -0600

    actualizacion

--- Practica3/Practica3.py
+++ Practica3/Practica3.py
@@ -5,19 +5,30 @@ from sys import path
 path.append("../MINERIA-DE-DATOS")
 import pandas as pd
 import matplotlib.pyplot as plt
-from Practica2.Practica2 import *
 import Practica2.Practica2 as P2
 
 P2.data
-P2.sel_cols
-P2.wards
-P2.gold
 
-plt.figure(figsize=(10,6))
-plt.subplot(131)
-plt.bar(P2.data['gameId'], P2.data['blueWins'])
-plt.subplot(131)
-plt.bar(P2.data['gameId'], P2.data['redWins'])
+plt.figure(figsize=(20,20))
+plt.subplot(1, 4, 1)
+plt.scatter(P2.data['blueKills'], P2.data['blueTotalGold'], c='blue')
+plt.title('EQUIPO AZUL', loc = "right", fontdict = {'fontsize':20, 'fontweight':'bold', 'color':'tab:blue'})
+plt.xlabel('Asesinatos', fontdict = {'fontsize':12, 'fontweight':'bold', 'color':'tab:blue'})
+plt.ylabel('Oro Total Ganado', fontdict = {'fontsize':12, 'fontweight':'bold', 'color':'tab:blue'})
+plt.subplot(1, 4, 2)
+plt.scatter(P2.data['blueTotalMinionsKilled'], P2.data['blueTotalGold'], c='blue')
+plt.xlabel('Asesinatos', fontdict = {'fontsize':12, 'fontweight':'bold', 'color':'tab:blue'})
+plt.ylabel('Total de Minions Asesinados', fontdict = {'fontsize':12, 'fontweight':'bold', 'color':'tab:blue'})
+plt.subplot(1, 4, 3)
+plt.scatter(P2.data['redKills'], P2.data['redTotalGold'], c='red')
+plt.xlabel('Asesinatos', fontdict = {'fontsize':12, 'fontweight':'bold', 'color':'tab:red'})
+plt.ylabel('Oro Total Ganado', fontdict = {'fontsize':12, 'fontweight':'bold', 'color':'tab:red'})
+plt.title('EQUIPO ROJO', loc = "right", fontdict = {'fontsize':20, 'fontweight':'bold', 'color':'tab:red'})
+plt.subplot(1, 4, 4)
+plt.scatter(P2.data['redTotalMinionsKilled'], P2.data['redTotalGold'], c='red')
+plt.xlabel('Total de Minions Asesinados', fontdict = {'fontsize':12, 'fontweight':'bold', 'color':'tab:red'})
+plt.ylabel('Oro Total Ganado', fontdict = {'fontsize':12, 'fontweight':'bold', 'color':'tab:red'})
+
 plt.show()
 
 
--- Practica3/tempCodeRunnerFile.py
+++ Practica3/tempCodeRunnerFile.py
@@ -1,22 +1,9 @@
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-# PRACTICA 3: DATA VISUALIZATION
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-from sys import path
-path.append("../MINERIA-DE-DATOS")
-import pandas as pd
-import matplotlib.pyplot as plt
-from Practica2.Practica2 import *
-import Practica2.Practica2 as P2
 
-P2.data
-P2.sel_cols
-P2.wards
-P2.gold
-
-plt.figure(figsize=(10,6))
-plt.subplot(131)
-plt.bar(P2.data['gameId'], P2.data['blueWin'])
-plt.subplot(131)
-plt.bar(P2.data['gameId'], P2.data['redWin'])
+plt.figure(figsize=(15,15))
+plt.subplot(1,2,1)
+plt.scatter(P2.data['blueKills'], P2.data['blueTotalGold'])
+plt.title('Equipo Azul')
+plt.subplot(1,2,2)
+plt.scatter(P2.data['redKills'], P2.data['redTotalGold'])
+plt.title('Equipo Rojo')
 plt.show()
-
