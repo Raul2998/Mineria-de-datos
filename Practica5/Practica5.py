cf41ab9dc14f4a8a19d6a59353106930e654e005
Raul Gonzalez <gabriel71871@gmail.com>
2023-05-11 23:59:53 -0600

    act

--- Practica5/Practica5.py
+++ Practica5/Practica5.py
@@ -7,29 +7,30 @@ from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
 
 data = pd.read_csv('C:/Users/raulh/OneDrive/Documentos/Mineria-de-datos/lol_dataframe2.csv')
-# Creating X and y
+
+# Creando eje X y eje Y
 x = data['blueKills']
 y = data['blueTotalGold']
 
-# Splitting the varaibles as training and testing
-x_train, X_test, y_train, y_test = train_test_split(x, y, train_size = 0.7, test_size = 0.3, random_state = 100)
+# Separando las variables en training y testing
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.7, test_size = 0.3, random_state = 100)
 
-# Take a look at the train dataset
+# Visualizamos el dataset train
 print(x_train,y_train)
 
-# Adding a constant to get an intercept
+# Agregamos una constante para obtener la interseccion
 X_train_sm = sm.add_constant(x_train)
 
-# Fitting the resgression line using 'OLS'
+# Ajustamos la regresion lineal usando'OLS'
 lr = sm.OLS(y_train, X_train_sm).fit()
 
-# Printing the parameters
+# Impresion de los parametros
 print(lr.params)
 
 # Performing a summary to list out all the different parameters of the regression line fitted
 print(lr.summary())
 
-# Visualizing the regression line
+# Visualizamos la regresion lineal
 plt.scatter(x_train, y_train)
 plt.plot(x_train,  1.368e+04  + 457.7481*x_train, 'r')
 plt.show()
